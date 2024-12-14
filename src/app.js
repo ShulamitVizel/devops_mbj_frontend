f6db53e99a7980c2bf421abe174d125b00ae2d9d
ShulamitVizel <shv3282@gmail.com>
2024-12-14 20:23:47 +0200

    a small change in the fetch data

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    fetch('http://34.149.149.146:5000/api')
+    fetch('http://34.54.143.51:80/api')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
